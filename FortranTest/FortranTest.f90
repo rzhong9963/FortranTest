42364f22660e70692af3d8c31516f389077c1748
Rob <rzhong9963@gmail.com>
2019-02-07 18:06:18 -0600

    Add project files.

---
+++ FortranTest/FortranTest.f90
@@ -0,0 +1,25 @@
+!  FortranTest.f90 
+!
+!  FUNCTIONS:
+!  FortranTest - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: FortranTest
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program FortranTest
+
+    implicit none
+
+    ! Variables
+
+    ! Body of FortranTest
+    print *, 'Hello World'
+
+    end program FortranTest
+
